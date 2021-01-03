fc8b9b6eb914a967e582f729d19cb51495e92898
Pwako11 <akolo@hotmail.com>
2021-01-03 13:13:54 -0500

    Built Create, read, delete

--- src/index.js
+++ src/index.js
@@ -1,47 +1,105 @@
-const reviewform = document.getElementById("review-form")
-const reviewTitle = document.getElementById("title")
-const reviewContent = document.getElementById("content")
-const reviewRating = document.getElementById("rating")
+const reviewForm = document.getElementById("review-form")
+const reviewInput = document.getElementById("review-input")
 const reviewList = document.getElementById("review-list")
+const reviewURL = `http://localhost:3000/reviews`
+const commentURL = `http://localhost:3000/comments`
 
-reviewform.addEventListener("submit", submitReview)
+function fetchReviews(){
+        fetch(reviewURL)
+        .then(resp => resp.json())
+        .then(reviews => reviews.forEach(data => renderReview(data.data)));
+        }
 
-function submitReview(e){
-    e.preventDefault()
+reviewForm.addEventListener ("submit", submitReview)
+
+
+function submitReview(){
+    event.preventDefault()
+   
+    const configObj = {
+        method: "POST",
+        headers:{
+            "Content-type": "application/json",
+            "Accept": "application/json"
+                },
+        body: JSON.stringify({
+           content: reviewInput.value
+        })
+    }
+    fetch(reviewURL, configObj)
+    .then(res => res.json())
+    .then(data => renderReview(data.data))   
+}
+
+// render review to dom 
+function renderReview(review){
+    console.log(review)
     const li = document.createElement('li')
+    li.dataset.id = review.id
+
     const p = document.createElement('p')
-    const commentList = document.createElement('ul')
-    
-    let arrValues = new Array();
-    arrValues.push([e.target.children[1].value, e.target.children[4].value, e.target.children[8].value]);
-    let post = arrValues.map(arrValue => `<li>${arrValue}</li>`).join('\n');
-    p.innerHTML = post
-    
+
+    p.innerText = review.attributes.content
+
+    const deleteBtn = document.createElement("button")
+    deleteBtn.innerText = "delete"
+    deleteBtn.addEventListener("click", deleteReview)
+
     const commentForm = document.createElement('form')
     commentForm.innerHTML += `<input type="text" id="commentInput"><input type="submit">`
+    commentForm.addEventListener("submit", renderComment)
     
-    commentForm.addEventListener("submit", submitComment)
+    const commentList = document.createElement('ul')
+    review.attributes.comments.forEach(comment => {
+        const commentLi = document.createElement('li')
+        commentLi.innerText = comment.content
+        commentList.appendChild(commentLi)
+    })
 
-    commentList.innerHTML
-
-    li.append(p,commentForm, commentList)
-    
+    li.append(p, deleteBtn, commentForm, commentList)
     reviewList.appendChild(li)
-    
-    reviewform.reset()
+
+    reviewForm.reset()
 }
 
-function renderReview(){
-    
+function deleteReview(e){
+    const reviewId = e.target.parentElement.dataset.id 
+    fetch(`${reviewURL}/${reviewId}`, {
+        method: "DELETE"
+       })    
+       e.target.parentElement.remove()
 }
 
-function submitComment(e){
+function renderComment(e){
     e.preventDefault()
     const commentInput = e.target.children[0].value
     const commentList = e.target.nextElementSibling
+    const reviewId = e.target.parentElement.dataset.id
+
     const li = document.createElement('li')
+    li.dataset.id = reviewId
     li.innerText = commentInput
     commentList.appendChild(li)
-    
+     
+    submitComment(commentInput, reviewId)
+
     e.target.reset()
 }
+
+function submitComment(comment, reviewId){
+    console.log(comment)
+    fetch(commentURL, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+           content: comment,
+           review_id: reviewId
+        })
+    }) 
+}
+
+
+fetchReviews()
